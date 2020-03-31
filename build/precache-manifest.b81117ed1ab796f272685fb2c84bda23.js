fad7272ac2bafb3c252238255c106415e929f8c0
Rohit <therohitjha@gmail.com>
2020-03-31 06:19:20 +0530

    first commit

---
+++ build/precache-manifest.b81117ed1ab796f272685fb2c84bda23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a37d04ed112780dc45b2e7b8fdec6d5",
+    "url": "/star-wars-planet-search/index.html"
+  },
+  {
+    "revision": "e0da85ce1cd42f481579",
+    "url": "/star-wars-planet-search/static/css/main.f129408b.chunk.css"
+  },
+  {
+    "revision": "029fd14be5182da7a3f9",
+    "url": "/star-wars-planet-search/static/js/2.a8d7b86e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/star-wars-planet-search/static/js/2.a8d7b86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0da85ce1cd42f481579",
+    "url": "/star-wars-planet-search/static/js/main.ef9855f4.chunk.js"
+  },
+  {
+    "revision": "69d6528c91fa3592c521",
+    "url": "/star-wars-planet-search/static/js/runtime-main.86ccc8b8.js"
+  },
+  {
+    "revision": "41ffd790a9e5853bf9f9b70529a3fbbc",
+    "url": "/star-wars-planet-search/static/media/loading.41ffd790.gif"
+  }
+]);
